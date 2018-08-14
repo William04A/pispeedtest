82398a120ac36a4cfb6e79937ba8b83b7acdad4d
William04A <35110380+William04A@users.noreply.github.com>
2018-08-14 15:54:05 +0200

    Updated to Automaticsetup 2.0 - Big improvents!
    The new setup is cross-platform (just not for Windows).

--- automaticsetup.py
+++ automaticsetup.py
@@ -1,8 +1,8 @@
 import os
 
-import subprocess
+import pip._internal
 
-print("PiSpeedtest Automatic Setup 1.0 (released with PiSpeedtest 2.0)")
+print("PiSpeedtest Automatic Setup 2.0 (released with PiSpeedtest 3.5)")
 
 
 with open(os.path.join(os.getcwd() + "\\configurationfiles\\" + "languageconfiguration.txt"), "r+") as languageconfigurationsfile:
@@ -11,23 +11,40 @@ with open(os.path.join(os.getcwd() + "\\configurationfiles\\" + "languageconfigu
 
 
 #Language configuarations
+
 if languageconfiguration == "sv-se":
     notwindowsos = "Du har inte Windows som operativsystem. Installeraren är tyvärr endast kompatibel med Windows i nuläget."
 
     installcomplete = "PiSpeedtest har nu installerats. Röd text innebär att något inte fungerade under installationen. Om du inte fick någon röd text så innebär det med största sannolikhet att PiSpeedtest har installerats korrekt."
+    pipupgradeinformationmessage = "\"pip\" (Python Pakethanterare) kommer att uppdateras. Vill du skippa det (rekommenderas ej)?  Nuvarande pip-version: "
+    installquestionmessage = "Skriv \"YES\" för att fortsätta eller skriv någonting annat för att skippa \"pip\"-uppdateringen: "
 elif languageconfiguration == "en-us":
     notwindowsos = "Your operating system is not Windows. The setup program is only compatible with Windows at the moment."
 
     installcomplete = "PiSpeedtest has now been installed. Any red text means that the installation failed. If you didn´t get any red text, it most likely means that PiSpeedtest´s requirements has been installed properly."
+    pipupgradeinformationmessage = "Your \"pip\" (Python Package List) will be updated. Do you want to skip that (not recommended)?  Current pip version: "
+    installquestionmessage = "Type \"YES\" to continue or type anything else to skip the pip update: "
 else:
     notwindowsos = "The current language configuration is somehow invalid."
 
     installcomplete = "The current language configuration is somehow invalid."
-if os.name == "nt":
-    updatepipcode = subprocess.Popen(["python -m pip"], shell=True)
-    installcode1 = subprocess.Popen(["pip install speedtest-cli"], shell=True)
-    installcode2 = subprocess.call(["pip install beautifulsoup4"], shell=True)
 
-    print(installcomplete)
+    pipupgradeinformationmessage = "The current language conifguration is somehow invalid."
+
+    installquestionmessage = "The current language configuration is somehow invalid."
+print(pipupgradeinformationmessage + str(pip.__version__) + ".")
+installquestion = input(installquestionmessage)
+
+if installquestion == "yes" or installquestion == "YES":
+    pip._internal.main(["install", "--upgrade", "pip"])
 else:
-    print(notwindowsos)
+    print("\n")
+print("\n" + "Please wait... (0% installed)")
+pip._internal.main(["install", "speedtest-cli"])
+print("\n" + "Please wait... (33% installed)")
+pip._internal.main(["install", "beautifulsoup4"])
+print("\n" + "Please wait... (66% installed)")
+pip._internal.main(["install", "requests"])
+print("\n" + "Please wait... (100% installed)")
+
+print(installcomplete)
